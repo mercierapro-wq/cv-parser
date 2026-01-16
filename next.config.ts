0f118d4d7e9aa47273a4f82cbe9be1d9a79bcd1a
mercierapro <nodalforge@gmail.com>
2026-01-16 15:01:58 +0100

    npm run fix

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
+import path from "path";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  turbopack: {
+    // Force la racine de Turbopack sur le dossier du projet
+    root: path.resolve(__dirname),
+  },
 };
 
 export default nextConfig;
